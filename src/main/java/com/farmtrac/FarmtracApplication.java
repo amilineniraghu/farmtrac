ae35d36093ef89edb8f41e2028b14f1a3c8892ce
Raghudeep Amilineni <raghu.amilineni@gmail.com>
2020-09-17 20:45:39 +0530

    added base springboot application

---
+++ src/main/java/com/farmtrac/FarmtracApplication.java
@@ -0,0 +1,13 @@
+package com.farmtrac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmtracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmtracApplication.class, args);
+	}
+
+}
