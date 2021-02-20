dcce7d85858a424dc44f439478f2aeb3e3c033a0
Raghudeep Amilineni <raghu.amilineni@gmail.com>
2021-02-20 21:04:24 +0530

    Request Param testing

--- src/main/java/com/farmtrac/controller/TestContoller.java
+++ src/main/java/com/farmtrac/controller/TestContoller.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,8 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestContoller{
 	 private static final Logger logger = LogManager.getLogger(TestContoller.class);
 	@GetMapping("/getList")
-	public String testGetMethod() {
-		logger.info("getList request is called");
+	public String testGetMethod(@RequestParam(value = "name") String name) {
+		logger.info("getList request is called" + name);
 		return "test";
 	}
 }
